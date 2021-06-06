5a3e76841a7be6c4a588f13022d5cdb4df5b9246
Rachid Amrani <rachid3alaoui@gmail.com>
2021-06-06 18:04:42 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const userNameField = document.querySelector('.username')
 const message = document.querySelector('.message')
 
-const usernamePattern = /[a-z][a-z0-9]{7,29}/i
+const usernamePattern = /^[a-z][a-z0-9]{7,29}$/i
 
 userNameField.addEventListener('input', usernameValidation)
 
 function usernameValidation() {
   if (usernamePattern.test(userNameField.value)) {
     message.textContent = showGood()
-    message.classList.add('good')
   } else {
     message.textContent = showBad()
   }
